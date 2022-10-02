096746f28b27ec29e3e5a936cd98c350f110e51e
gnarlynarley <gnarlynarley@gmail.com>
2022-10-03 00:53:54 +0200

    feat: add deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "@rollup/plugin-commonjs";
 
+const publicPath = "/gif-study/";
+
 export default defineConfig({
+  base: publicPath,
   plugins: [react(), commonjs()],
 });
