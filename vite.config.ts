f89053bc326ae76cdd4082ed4811ef70fc92a318
Narley <gnarlynarley@gmail.com>
2026-07-24 23:40:45 +0200

    feat: add analyze flag

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { analyzer } from "vite-bundle-analyzer";
 
+const ANALYZE = false;
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), analyzer()],
+  plugins: [svelte(), analyzer({ enabled: ANALYZE })],
   resolve: { tsconfigPaths: true },
   base: "/gif-study/",
 });
