0f2d7cf130b7d32e3bbddeaf581e4d78646b5255
Narley <gnarlynarley@gmail.com>
2026-07-31 10:45:30 +0200

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { analyzer } from "vite-bundle-analyzer";
+import { VitePWA } from "vite-plugin-pwa";
 
 const ANALYZE = false;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), analyzer({ enabled: ANALYZE })],
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      pwaAssets: {
+        config: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        theme_color: "#e2ad00",
+      },
+    }),
+    svelte(),
+    analyzer({ enabled: ANALYZE }),
+  ],
   resolve: { tsconfigPaths: true },
   base: "/gif-study/",
 });
